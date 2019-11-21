32a584818e21b1a50f222cb6b687fa7e79eb99c3
Nóra Máté <norama.matema@gmail.com>
2019-11-21 23:20:17 +0100

    Updates

---
+++ precache-manifest.766e72e18320111f7bf162540ed198d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81f7781a4c16bc20465a77735ed2772",
+    "url": "/selector/index.html"
+  },
+  {
+    "revision": "ea5cc111ebaa5def2f01",
+    "url": "/selector/static/css/2.f66b4398.chunk.css"
+  },
+  {
+    "revision": "5c32dfa70a9428d31d3e",
+    "url": "/selector/static/css/main.601ad859.chunk.css"
+  },
+  {
+    "revision": "ea5cc111ebaa5def2f01",
+    "url": "/selector/static/js/2.01b106be.chunk.js"
+  },
+  {
+    "revision": "5c32dfa70a9428d31d3e",
+    "url": "/selector/static/js/main.a1af03c5.chunk.js"
+  },
+  {
+    "revision": "29e5a9de25c8de5ad372",
+    "url": "/selector/static/js/runtime-main.0b356993.js"
+  }
+]);
